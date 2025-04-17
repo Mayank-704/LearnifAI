a54bd2aef54c3d9bc466fbea8e2247fbeabc8bff
Mayank Gupta <mayankg88813@gmail.com>
2025-04-17 12:28:49 +0530

    TTS service working smoothly

--- backend/src/routes/groq.route.js
+++ backend/src/routes/groq.route.js
@@ -1,10 +1,13 @@
 // routes/groqRoutes.js
 import express from 'express';
 import { handleGroqQuery } from '../controllers/groq.controller.js'
+import { generateGroqSpeech } from '../controllers/groqTTS.controller.js';
 // import verifyFirebaseToken from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/ask', handleGroqQuery);
+router.post('/tts', generateGroqSpeech);
+
 
 export default router;
