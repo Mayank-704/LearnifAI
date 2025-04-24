23c3055c1dd1182bff0b185249f48d63930960d7
MohammadDishan <mohammaddishan0369@gmail.com>
2025-04-24 16:32:31 +0530

    Update README.md

--- extension/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getSelectedText") {
-      const selectedText = window.getSelection().toString();
-      sendResponse({ text: selectedText });
-    }
-  });
